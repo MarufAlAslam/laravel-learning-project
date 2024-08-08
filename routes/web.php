bf8cf7024655e99f94b5886ce2287aada9d4e58e
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-08-09 02:52:48 +0600

    single param with route added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\SiteController@Home');
-Route::get('/about', 'App\Http\Controllers\SiteController@About');
-Route::get('/contact', 'App\Http\Controllers\SiteController@Contact');
+Route::get("/name/{nameValue}", 'App\Http\Controllers\DemoController@MyName');
