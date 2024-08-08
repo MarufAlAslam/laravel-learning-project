bcbc6adf6aec8f5155d08b3ecb9df83e07b62624
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-08-09 03:21:46 +0600

    routing group done

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("/name/{firstName}/{middleName}/{lastName}", 'App\Http\Controllers\DemoController@MyName');
+
+// Route::get('/account/profile','');
+// Route::get('/account/login','');
+// Route::get('/account/logout','');
+// Route::get('/account/signup','');
+// Route::get('/account/update-profile','');
+
+// routing group
+Route::group(['prefix' => 'account'], function () {
+    Route::get('/profile', function () {
+        return "Profile";
+    });
+    Route::get('/login', function () {
+        return "Login";
+    });
+    Route::get('/logout', function () {
+        return "Logout";
+    });
+    Route::get('/signup', function () {
+        return "Signup";
+    });
+    Route::get('/update-profile', function () {
+        return "Update Profile";
+    });
+});
